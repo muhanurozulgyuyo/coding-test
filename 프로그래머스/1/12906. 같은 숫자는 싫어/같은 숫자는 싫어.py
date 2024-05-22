9ea92fa4809cbd8ebbb84b9c0c36d7de6ea1e58c
muhanurozulgyuyo <158283090+muhanurozulgyuyo@users.noreply.github.com>
2024-05-22 22:04:30 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 279.23 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            return None
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+def solution(arr):
+    S = Stack()
+    
+    for i in arr:
+        if S.peek() != i:
+            S.push(i)
+    
+    return S.items
