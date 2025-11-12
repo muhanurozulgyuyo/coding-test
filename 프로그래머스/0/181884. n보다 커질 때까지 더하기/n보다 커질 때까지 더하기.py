022de95afa64bb6881f72fa6eb738ba5944d30af
Yeongmin Maeng <158283090+muhanurozulgyuyo@users.noreply.github.com>
2025-11-12 22:33:29 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
@@ -0,0 +1,8 @@
+def solution(numbers, n):
+    answer = 0
+    for i in range(len(numbers)):
+        if answer <= n:
+            answer += numbers[i]
+        else:
+            break
+    return answer
