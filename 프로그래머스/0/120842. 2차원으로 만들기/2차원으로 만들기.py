f74cf1f066b7bec1e2057c7e0a439d2c17480c14
Yeongmin Maeng <158283090+muhanurozulgyuyo@users.noreply.github.com>
2026-02-03 20:13:06 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,12 +1,11 @@
 def solution(num_list, n):
-    s = len(num_list) // n
-    answer = []
-    
-    for i in range(s):
-        li = []
-        for j in range(n):
-            a = num_list.pop(0)
-            li.append(a)
-        answer.append(li)
-    
-    return answer
+    li = []
+    tmp = []
+
+    for i in range(len(num_list)):
+        tmp.append(num_list[i])
+        if len(tmp) == n:
+            li.append(tmp)
+            tmp = []
+
+    return li
