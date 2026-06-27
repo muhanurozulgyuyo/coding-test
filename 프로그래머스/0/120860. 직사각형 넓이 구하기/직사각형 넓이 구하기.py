44caccc8b58c697686f0220ea511569e8e6f2f81
Yeongmin Maeng <158283090+muhanurozulgyuyo@users.noreply.github.com>
2026-06-27 16:28:28 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -0,0 +1,8 @@
+def solution(dots):
+    answer = 0
+    x = []
+    y = []
+    for i in range(len(dots)):
+        x.append(dots[i][0])
+        y.append(dots[i][1])
+    return abs((max(x) - min(x))*(max(y) - min(y)))
